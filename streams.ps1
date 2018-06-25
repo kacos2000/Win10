3dd8e48d20c800fd3bd5867166b027b3a9794799
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-06-25 05:27:26 +0300

    Add files via upload

--- streams.ps1
+++ streams.ps1
@@ -23,17 +23,15 @@ $results = ForEach ($File in $Files)
 	Filename = $File.FullName 
 	Owner = (Get-Acl $file.FullName).owner
 	Attributes = $File| Select length, lastwritetime, attributes
-	Stream = (Get-Item $File.FullName -Stream *).stream |Out-String 
-	StreamContents = Get-Content -Stream Zone.Identifier $File.FullName -ErrorAction "Ignore" |Out-String 
+	Stream = (Get-Item $File.FullName -Stream *).stream |Out-String
+	StreamContents = Get-Content -Stream Zone.Identifier $File.FullName -ErrorAction "Ignore" |Out-String
     }
 	
  }
 #Format of the CSV filename and path:
-$filenameFormat = $env:userprofile + "\desktop\streams" + " "  + (Get-Date -Format "dd-MM-yyyy hh-mm") + ".csv"
+$filenameFormat = $env:userprofile + "\desktop\streams" + " "  + (Get-Date -Format "dd-MM-yyyy hh-mm") + ".txt"
 
-#Output results to screen table (and saves selected rows to CSV) - save needs work
-$results|Out-GridView -PassThru -Title "File Zone.Identifier Stream Contents" 
-
-# |Export-Csv -Path $filenameFormat -NoTypeInformation -Encoding Unicode
+#Output results to screen table (and saves selected rows to CSV) 
+$results|Out-GridView -PassThru -Title "File Zone.Identifier Stream Contents" |Out-File -FilePath $filenameFormat -Encoding Unicode
 
 

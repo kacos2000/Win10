7977621957ac85089745457f44f828db0c49e83a
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-07-02 13:23:14 +0300

    Add files via upload

--- streams.ps1
+++ streams.ps1
@@ -1,3 +1,7 @@
+# ref: https://blogs.technet.microsoft.com/askcore/2013/03/24/alternate-data-streams-in-ntfs/
+#
+#
+#
 # Show a Select Folder Dialog and return the folder selected by the user
 Function Get-Folder($initialDirectory)
 {
@@ -10,28 +14,40 @@ Function Get-Folder($initialDirectory)
     }
     return $folder
 }
+
 $Folder = Get-Folder
 
 #Enumerate the files in the selected folder ( if -recurse below, recursively)
 $Files = Get-ChildItem -Path $Folder -recurse
 
 
+			  
 #Display the following results for each file in the Directory:
-$results = ForEach ($File in $Files)
-{
+$results = ForEach ($File in $Files) {
+
+	try{$Stream = (Get-Item $File.FullName -Stream *).stream|out-string}
+		Catch{$Stream = ""|out-string}
+	try{$Zone = Get-Content -Stream Zone.Identifier $File.FullName -ErrorAction "Ignore" |Out-String}
+		Catch{$Zone =""|out-string}
+	 
+		
+	
 	[PSCustomObject]@{ 
 	Filename = $File.FullName 
 	Owner = (Get-Acl $file.FullName).owner
-	Attributes = $File| Select length, lastwritetime, attributes
-	Stream = (Get-Item $File.FullName -Stream *).stream |Out-String
-	StreamContents = Get-Content -Stream Zone.Identifier $File.FullName -ErrorAction "Ignore" |Out-String
-    }
+	Length = (Get-ItemProperty $File.FullName).length
+	LastWriteTime = (Get-ItemProperty $File.FullName).lastwritetime
+	Attributes = (Get-ItemProperty $File.FullName).Mode
+	Stream = $Stream
+	ZoneIdContents = $Zone
+	}
 	
  }
-#Format of the CSV filename and path:
+
+#Format of the txt filename and path:
 $filenameFormat = $env:userprofile + "\desktop\streams" + " "  + (Get-Date -Format "dd-MM-yyyy hh-mm") + ".txt"
 
-#Output results to screen table (and saves selected rows to CSV) 
+#Output results to screen table (and saves selected rows to txt) 
 $results|Out-GridView -PassThru -Title "File Zone.Identifier Stream Contents" |Out-File -FilePath $filenameFormat -Encoding Unicode
-
+[gc]::Collect()
 

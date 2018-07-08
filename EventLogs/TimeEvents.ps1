db8a63f7283ba9379553628a718d2e6dae1eaaba
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-07-08 23:36:12 +0300

    Add files via upload

--- EventLogs/TimeEvents.ps1
+++ EventLogs/TimeEvents.ps1
@@ -30,11 +30,10 @@ $DesktopPath = ($Env:WinDir+"\System32\winevt\Logs\")
 
 $File = $Folder + "security.evtx"
 Write-Host "(TimeEvents.ps1):" -f Yellow -nonewline; write-host " Selected Event Log: ($File)" -f White
-$1=1
+$c=1
 
 Try {
 	$Event = (Get-WinEvent -FilterHashtable @{path = $File; ID=4616} -ErrorAction Stop)    
-	Write-host "Total events with ID: 4616 = " -f Yellow -nonewline; Write-Host $Event.count -f White
 	$log = (Get-WinEvent -FilterHashtable @{path = $File; ID=4616})
 	}
 	catch [Exception] {
@@ -44,21 +43,24 @@ Try {
 
 [xml[]]$xmllog = $log.toXml()
 
-$Events = foreach ($i in $xmllog) {
+$Events = foreach ($i in $xmllog) {$c++
+			Write-Progress -Activity "Collecting event entries with ID = 4616" -Status "Entry $c of $($xmllog.Count))" -PercentComplete (($c / $xmllog.Count)*100)
 			$Previous = [DateTime] ($i.Event.EventData.Data[4].'#text')
 			$New = [DateTime] ($i.Event.EventData.Data[5].'#text')
-
+						
+			# Format output fields
+			
 			[PSCustomObject]@{ 
 			'Time Created' = Get-Date ($i.Event.System.TimeCreated.SystemTime) -format o
-			EventRecordID = $i.Event.System.EventRecordID
-			ProcessID = $i.Event.System.Execution.ProcessID
-			ThreadID = $i.Event.System.Execution.ThreadID
+			'EventID' = $i.Event.System.EventRecordID
+			'PID' = $i.Event.System.Execution.ProcessID
+			'ThreadID' = $i.Event.System.Execution.ThreadID
 			'User Name' = $i.Event.EventData.Data[1].'#text'
 			'SID' = $i.Event.EventData.Data[0].'#text'
 			'Domain Name' = $i.Event.EventData.Data[2].'#text'
-			'New Time' = Get-Date ($i.Event.EventData.Data[5].'#text') 
-			'Previous Time' = Get-Date ($i.Event.EventData.Data[4].'#text') 
-			Change = ($New - $Previous) 
+			'New Time UTC' = Get-Date ($i.Event.EventData.Data[5].'#text') 
+			'Previous Time UTC' = Get-Date ($i.Event.EventData.Data[4].'#text') 
+			'Change' = ($New - $Previous) 
 			'Process Name' = $i.Event.EventData.Data[7].'#text'
 			}
 	}
@@ -68,6 +70,6 @@ $filenameFormat = $env:userprofile + "\desktop\TimeEvents_" + (Get-Date -Format 
 Write-host "Selected Rows will be saved as: " -f Yellow -nonewline; Write-Host $filenameFormat -f White
 
 #Output results to screen table (and saves selected rows to txt) 		
-$Events|Out-GridView -PassThru -Title "$File events related to ID 4616 (The system time was changed)"|Export-Csv -Path $filenameFormat 
-notepad $filenameFormat
+$Events|Out-GridView -PassThru -Title "$File events related to ID 4616 (The system time was changed)"
+#notepad $filenameFormat
 [gc]::Collect() 

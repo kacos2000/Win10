82246d167242c2a7acaf57625b367b74cecf0547
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-07-13 19:32:21 +0300

    Add files via upload

--- Bam/bamoffline.ps1
+++ Bam/bamoffline.ps1
@@ -1,15 +1,5 @@
 #Requires -RunAsAdministrator
 
-<#
-.Original Script source 
-	https://github.com/mgreen27/Powershell-IR/blob/master/Content/Other/BAMParser.ps1
-#>
-
-[CmdletBinding()]
-Param(
-    [Parameter(Mandatory = $False)][Switch]$SortSid=$Null
-)
-
 # Show an Open File Dialog and return the file selected by the user
 Function Get-FileName($initialDirectory)
 
@@ -26,13 +16,11 @@ $OpenFileDialog.ShowHelp = $false
 } #end function Get-FileName 
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 
-#  Note: OpenFile will always open the file in read-only mode.
-#  https://technet.microsoft.com/en-us/library/system.windows.forms.openfiledialog.openfile(v=vs.100)
 $File = Get-FileName -initialDirectory $DesktopPath
 
 Try{$before = (Get-FileHash $File -Algorithm SHA256).Hash}
 Catch{
-        Write-Host "(BamOffline.ps1):" -f Yellow -nonewline; Write-Host " User Cancelled" -f White
+        Write-Host "(Streams.ps1):" -f Yellow -nonewline; Write-Host " User Cancelled" -f White
 		exit
 }
 write-host "SHA256 Hash of ($File) before access = " -f magenta -nonewline;write-host "($before)" -f Yellow
@@ -62,11 +50,11 @@ Catch{
 $UserTime = (Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").TimeZoneKeyName
 $UserBias = (Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").ActiveTimeBias
 $UserDay = (Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").DaylightBias
-$1=1
+$u=0
 
-$result = Foreach ($Sid in $Users){$i++
+$result = Foreach ($Sid in $Users){$u++
     $Items = Get-Item -Path "HKLM:\Temp\ControlSet001\Services\bam\UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
-
+	$i = 0 
     # Enumerating User - will roll back to SID on error
     Try{
         $objSID = New-Object System.Security.Principal.SecurityIdentifier($Sid) 
@@ -74,24 +62,24 @@ $result = Foreach ($Sid in $Users){$i++
         $User = $User.Value
     }
     Catch{$User=""}
-	Write-Progress -Activity "Collecting BAM entries for each User (sid)" -Status "Entry $i of $($Users.Count))" -PercentComplete (($i / $Users.Count)*100)
-    ForEach ($Item in $Items){
+	Write-Progress -id 1 -Activity "Collecting Security ID (sid) entries" -Status "SID $u of $($Users.Count))" -PercentComplete (($u / $Users.Count)*100)
+    ForEach ($Item in $Items){$i++
 		$Key = Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Services\bam\UserSettings\$Sid" | Select-Object -ExpandProperty $Item
-        
+        Write-Progress -id 2 -Activity "Collecting BAM entries for each User (sid)" -Status "Entry $i of $($Items.Count))"  -ParentId 1
 		
         If($key.length -eq 24){
             $Hex=[System.BitConverter]::ToString($key[7..0]) -replace "-",""
             $TimeLocal = Get-Date ([DateTime]::FromFileTime([Convert]::ToInt64($Hex, 16))) -Format o
 			$TimeUTC = Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))) -Format u
-			$Bias = ([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
-			$Day = ([convert]::ToInt32([Convert]::ToString($UserDay,2),2))
-			$TImeUser = (Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))).addminutes(-$Bias) -Format s ) 
+			$Bias = -([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
+			$Day = -([convert]::ToInt32([Convert]::ToString($UserDay,2),2))
+			$TImeUser = (Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))).addminutes($Bias) -Format s ) 
 
             [PSCustomObject]@{
                         'Last Execution Time (UTC)'= $TimeUTC
 						'User Timezone' = $UserTime
-						'ActiveBias' = -$Bias
-						'Daylight'= -$Day
+						'ActiveBias' = $Bias/60
+						'Daylight'= $Day/60
 						'User Time' = $TImeUser
 						Application = $Item
 						User = $User
--- Bam/bamoffline1.ps1
+++ Bam/bamoffline1.ps1
@@ -1,9 +1,4 @@
 #Requires -RunAsAdministrator
-<#
-.Original Script source 
-	https://github.com/mgreen27/Powershell-IR/blob/master/Content/Other/BAMParser.ps1
-#>
-[gc]::Collect()	
 
 # Show an Open File Dialog and return the file selected by the user
 Function Get-FileName($initialDirectory)
@@ -20,9 +15,6 @@ $OpenFileDialog.ShowHelp = $false
 } #end function Get-FileName 
 
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
-
-#  Note: OpenFile will always open the file in read-only mode.
-#  https://technet.microsoft.com/en-us/library/system.windows.forms.openfiledialog.openfile(v=vs.100)
 
 $File = Get-FileName -initialDirectory $DesktopPath
  
@@ -50,14 +42,14 @@ Catch{
     exit
 }
 
-$UserTime = (Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").TimeZoneKeyName
+$UserTime = ([TimeZoneInfo]::FindSystemTimeZoneById((Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").TimeZoneKeyName)).DisplayName
 $UserBias = (Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").ActiveTimeBias
 $UserDay = (Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Control\TimeZoneInformation").DaylightBias
-$1=1
+$u=0
 
-$result = Foreach ($Sid in $Users){$i++
+$result = Foreach ($Sid in $Users){$u++
     $Items = Get-Item -Path "HKLM:\Temp\ControlSet001\Services\bam\UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
-
+	$i = 0 
     # Enumerating User - will roll back to SID on error
     Try{
         $objSID = New-Object System.Security.Principal.SecurityIdentifier($Sid) 
@@ -65,17 +57,20 @@ $result = Foreach ($Sid in $Users){$i++
         $User = $User.Value
     }
     Catch{$User=""}
-	Write-Progress -Activity "Collecting BAM entries for each User (sid)" -Status "Entry $i of $($Users.Count))" -PercentComplete (($i / $Users.Count)*100)
-    ForEach ($Item in $Items){
+	Write-Progress -id 1 -Activity "Collecting Security ID (sid) entries" -Status "SID $u of $($Users.Count))" -PercentComplete (($u / $Users.Count)*100)
+    
+	ForEach ($Item in $Items){$i++
 		$Key = Get-ItemProperty -Path "HKLM:\Temp\ControlSet001\Services\bam\UserSettings\$Sid" | Select-Object -ExpandProperty $Item
-        
-		
+        Write-Progress -id 2 -Activity "Collecting BAM entries for each User (sid)" -Status "Entry $i of $($Items.Count))"  -ParentId 1 -PercentComplete (([double]$i / $items.Count)*100)
+	
         If($key.length -eq 24){
             $Hex=[System.BitConverter]::ToString($key[7..0]) -replace "-",""
             $TimeLocal = Get-Date ([DateTime]::FromFileTime([Convert]::ToInt64($Hex, 16))) -Format o
 			$TimeUTC = Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))) -Format u
 			$Bias = -([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
-			$Day = -([convert]::ToInt32([Convert]::ToString($UserDay,2),2))
+			$Day = -([convert]::ToInt32([Convert]::ToString($UserDay,2),2)) 
+			$Biasd = $Bias/60
+			$Dayd = $Day/60
 			$TImeUser = (Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))).addminutes($Bias) -Format s) 
 			$d = if((((split-path -path $item) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
 			{((split-path -path $item).Remove(23)).trimstart("\Device\HarddiskVolume")} else {$d = ""}
@@ -100,14 +95,14 @@ $result = Foreach ($Sid in $Users){$i++
 }
 
 # Output to Window
-$result |Out-GridView -PassThru -Title "BAM key entries of ($File) - User TimeZone: ($UserTime) -> ActiveBias: ( $Bias) - DayLightTime: ($Day)"
+$result |Out-GridView -PassThru -Title "BAM key entries of ($File) - User TimeZone: $UserTime -> ActiveBias (TimeZone +- DayLightTime)= $Biasd - DayLightTime: $Dayd" 
 
 [gc]::Collect()		
 reg unload HKEY_LOCAL_MACHINE\Temp 
 $after = (Get-FileHash $File -Algorithm SHA256).Hash 
 write-host "SHA256 Hash of ($File) after access = " -f magenta -nonewline;write-host "($after)" -f Yellow
 $result = (compare-object -ReferenceObject $before -DifferenceObject $after -IncludeEqual).SideIndicator 
-write-host "The before and after SHA256 Hashes of ($File) are ($result) `n `n" -ForegroundColor White
+write-host "The before and after SHA256 Hashes of ($File) are ($result) `n `n" -ForegroundColor White 
 
 
 

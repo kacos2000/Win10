0277d459c95766bdff97488eafd0874be6dd4502
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-07-22 01:02:44 +0300

    Update bam.ps1

--- Bam/bam.ps1
+++ Bam/bam.ps1
@@ -15,10 +15,9 @@
     http://windowsir.blogspot.com.au/2018/03/new-and-updated-plugins-other-items.html
     http://batcmd.com/windows/10/services/bam/
 #>
+$sw = [Diagnostics.Stopwatch]::StartNew()
 
-
-$Users=$null
-
+#Getting BAM Entries
 
 # MAIN
 if (!(Get-PSDrive -Name HKLM -PSProvider Registry)){
@@ -35,51 +34,61 @@ Catch{
 $UserTime = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation").TimeZoneKeyName
 $UserBias = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation").ActiveTimeBias
 $UserDay = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation").DaylightBias
-$u=0
-$result = Foreach ($Sid in $Users){$u++
-    $Items = Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\bam\UserSettings\$Sid"-ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
-	Write-Progress -id 1 -Activity "Collecting Security ID (sid) entries" -Status "SID $u of $($Users.Count))" -PercentComplete (($u / $Users.Count)*100)
-    $i = 0 
+
+$Bam = Foreach ($Sid in $Users){$u++
+       $BamItems = Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\bam\UserSettings\$Sid" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
+       Write-Progress -id 1 -Activity "Get BAM entries" -Status "Collecting Security ID (sid) entries - SID $u of $($Users.Count))" -PercentComplete (($u / $Users.Count)*100) 
+       $bi = 0 
 	
-	# Enumerating User - will roll back to SID on error
-    Try{
+       # Enumerating User - will roll back to SID on error
+        Try{
         $objSID = New-Object System.Security.Principal.SecurityIdentifier($Sid) 
         $User = $objSID.Translate( [System.Security.Principal.NTAccount]) 
         $User = $User.Value
-    }
-    Catch{$User=""}
-
-    Foreach ($Item in $Items){$i++
-        $Key = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\bam\UserSettings\$Sid" | Select-Object -ExpandProperty $Item
-        Write-Progress -id 2 -Activity "Collecting BAM entries for each User (sid)" -Status "Entry $i of $($Items.Count))"  -ParentId 1 -PercentComplete (([double]$i / $items.Count)*100)
+        }
+        Catch{$User=""}
+        
+        Foreach ($BamItem in $BamItems){$bi++
+        $Key = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\bam\UserSettings\$Sid" | Select-Object -ExpandProperty $BamItem
+        Write-Progress -id 2 -Activity "Collecting BAM entries for each User (sid)" -Status "Entry $bi of $($BamItems.Count))" -PercentComplete (([double]$bi / $BamItems.Count)*100) -ParentID 1
 		
         If($key.length -eq 24){
             $Hex=[System.BitConverter]::ToString($key[7..0]) -replace "-",""
             $TimeLocal = Get-Date ([DateTime]::FromFileTime([Convert]::ToInt64($Hex, 16))) -Format o
 			$TimeUTC = Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))) -Format u
-			$Bias = ([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
-			$Day = ([convert]::ToInt32([Convert]::ToString($UserDay,2),2))
-			$TImeUser = (Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))).addminutes(-$Bias) -Format s) 
+			$Bias = -([convert]::ToInt32([Convert]::ToString($UserBias,2),2))
+			$Day = -([convert]::ToInt32([Convert]::ToString($UserDay,2),2))
+			$TImeUser = (Get-Date ([DateTime]::FromFileTimeUtc([Convert]::ToInt64($Hex, 16))).addminutes($Bias) -Format s) 
+			
+			$d = if((((split-path -path $BamItem) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
+			{((Split-path -path $BamItem).Remove(23)).Trimstart("\Device\HarddiskVolume")} else {$d = ""}
+
+			$f = if((((split-path -path $BamItem) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
+			{Split-path -leaf ($BamItem).TrimStart()} else {$item}		
+
+			$cp = if((((split-path -path $BamItem) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
+			{($BamItem).Remove(1,23)} else {$cp = ""}			
+			
+			$path = if((((split-path -path $BamItem) | ConvertFrom-String -Delimiter "\\").P3)-match '\d{1}')
+			{"(Vol"+$d+") "+$cp} else {$path = ""}
 			
             [PSCustomObject]@{
-                        'Last Execution Time (UTC)'= $TimeUTC
-						'User Timezone' = $UserTime
-						'ActiveBias' = -$Bias
-						'Daylight'= -$Day
-						'User Time' = $TImeUser
-						Application = $Item
+                        Last_Execution_Time_UTC= $TimeUTC
+						Local_Time = $TimeLocal
+						User_Time = $TimeUser
+						Application = $f
+						Full_Path = $path
+						Bam_Entry = $BamItem
 						User = $User
 						Sid = $Sid
-						}
-					 	
-		        }
-				
-   }
-   
-}
-
-$result |Out-GridView -PassThru -Title "BAM key entries"
-[gc]::Collect()
+						}}}}
+           
 
 
 
+
+$Bam|Out-GridView -PassThru -Title "BAM key entries  - User TimeZone: ($UserTime) -> ActiveBias: ( $Bias) - DayLightTime: ($Day)"
+
+$sw.stop()
+$t=$sw.Elapsed.TotalMinutes
+write-host "Elapsed Time $t minutes" -f yellow

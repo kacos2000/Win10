c061d01c064cf0b322a93a184e7f519131c14979
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-08-03 11:25:25 +0300

    Add files via upload

---
+++ Notifications/Notifications_WPNdatabase.sql
@@ -0,0 +1,17 @@
+select
+Notification.Id,
+Notification.HandlerId as 'H_Id',
+NotificationHandler.PrimaryId as 'Application',
+NotificationHandler.HandlerType,
+Notification.Type as 'Type',
+replace(replace(replace(Notification.Payload, char(13,10,32,32,32,32,32,32,32,32),''),char(32,32),''),char(32,32,32,32),'') as 'Payload',
+Notification.PayloadType as 'PayloadType',
+Notification.Tag as 'Tag',
+datetime((Notification.ArrivalTime - 116444736000000000)/10000000, 'unixepoch') as ArrivalTime,
+case when Notification.ExpiryTime = 0 then 'Expired' else datetime((Notification.ExpiryTime - 116444736000000000)/10000000, 'unixepoch') end as ExpiryTime,
+NotificationHandler.CreatedTime as 'H_Created',
+NotificationHandler.ModifiedTime as 'H_Modified',
+hex(Notification.ActivityId) as 'ActivityId'
+from Notification
+Join NotificationHandler on NotificationHandler.RecordId = Notification.HandlerId
+order by Id desc

9f7b535ec46627d1d6d36608fbadbfd85eaaa215
Costas K <11378310+kacos2000@users.noreply.github.com>
2018-08-03 23:13:03 +0300

    Add files via upload

--- Notifications/Notifications_WPNdatabase.sql
+++ Notifications/Notifications_WPNdatabase.sql
@@ -1,17 +1,24 @@
 select
-Notification.Id,
-Notification.HandlerId as 'H_Id',
+Notification.Id as 'ID',
+Notification.HandlerId as 'Handler_Id',
 NotificationHandler.PrimaryId as 'Application',
-NotificationHandler.HandlerType,
+NotificationHandler.HandlerType as 'HandlerType',
 Notification.Type as 'Type',
 replace(replace(replace(Notification.Payload,char(13)||char(10),''),'  ',''),char(9),'') as 'Payload', 
 Notification.PayloadType as 'PayloadType',
 Notification.Tag as 'Tag',
-datetime((Notification.ArrivalTime - 116444736000000000)/10000000, 'unixepoch') as ArrivalTime,
-case when Notification.ExpiryTime = 0 then 'Expired' else datetime((Notification.ExpiryTime - 116444736000000000)/10000000, 'unixepoch') end as ExpiryTime,
-NotificationHandler.CreatedTime as 'H_Created',
-NotificationHandler.ModifiedTime as 'H_Modified',
+datetime((Notification.ArrivalTime - 116444736000000000)/10000000, 'unixepoch') as 'ArrivalTime',
+case when Notification.ExpiryTime = 0 then 'Expired' else datetime((Notification.ExpiryTime - 116444736000000000)/10000000, 'unixepoch') end as 'ExpiryTime',
+NotificationHandler.CreatedTime as 'Handler_Created',
+NotificationHandler.ModifiedTime as 'Handler_Modified',
+NotificationHandler.WNSId as 'WNSId',
+NotificationHandler.WNFEventName as 'WNFEventName',
+WNSPushChannel.ChannelId as 'Channel_ID',
+WNSPushChannel.Uri as 'Uri',
+datetime((WNSPushChannel.CreatedTime - 116444736000000000)/10000000, 'unixepoch') as 'WNS_CreatedTime',
+datetime((WNSPushChannel.ExpiryTime - 116444736000000000)/10000000, 'unixepoch') as 'WNS_ExpiryTime',
 hex(Notification.ActivityId) as 'ActivityId'
 from Notification
 Join NotificationHandler on NotificationHandler.RecordId = Notification.HandlerId
+Left Join WNSPushChannel on WNSPushChannel.HandlerId = NotificationHandler.RecordId
 order by Id desc
